3bd2d5caee4d4c4e76fa515c6559bc77926c2554
yvoievid <yurii.voievidka@gmail.com>
2024-01-01 13:35:00 +0200

    renaming project

--- app/hello.py
+++ app/hello.py
@@ -7,7 +7,7 @@ config.read('config.properties')
 app = Flask(__name__)
 
 if config.getboolean("features", "feature_1") == True:
-	message = "Hello, Sasha!"
+	message = "Hello, Yurii!"
 else:
 	message = "Hello, World!"
 
