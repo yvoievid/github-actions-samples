3f27cfc6f8157fb03345706682252bf8f8a449ad
yvoievid <yurii.voievidka@gmail.com>
2024-01-03 22:27:34 +0200

    Added blue and green workflows for simple flask service, added unit tests, added environment variables for green and blue deployments for GKE

--- app/hello.py
+++ app/hello.py
@@ -1,5 +1,6 @@
 import configparser
 from flask import Flask
+import os
 
 config = configparser.RawConfigParser()
 config.read('config.properties')
@@ -9,12 +10,12 @@ app = Flask(__name__)
 if config.getboolean("features", "feature_1") == True:
 	message = "Hello, Yurii!"
 else:
-	message = "Hello, World!"
+	message = "Hello, User! Current version is " + str(os.getenv('VERSION'))
 
 @app.route("/")
 def hello():
 	return message 
 
-
+ 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8050)
